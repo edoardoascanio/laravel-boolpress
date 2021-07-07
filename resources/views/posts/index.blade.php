0add5b1c177653cd26da16a651b2a8a6879ce5f2
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-07-07 12:06:45 +0200

    add controller

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.public')
+
+@section('content')
+<h1>Public</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,18 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'HomeController@index')->name('index');
+Route::get('/posts', 'PostController@index')->name('posts.index');
 
 Route::prefix('admin')
-   ->namespace('Admin')
-   ->middleware('auth')
-   ->group(function (){
-        Route::get('/','HomeController@index')
-        ->name('home');
-   });
+     ->namespace('Admin')
+     ->middleware('auth')
+     ->name('admin.')
+     ->group(function (){
+          Route::get('/','HomeController@index')
+          ->name('index');
 
+          Route::resource('/posts', 'PostController');
+     });
 
-Route::prefix('users')
-   ->middleware('auth')
-   ->group(function (){
-        Route::get('/','UserController@index')
-        ->name('user');
-   });
 

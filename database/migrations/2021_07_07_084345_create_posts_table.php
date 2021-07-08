69fdde8380b3b2bb79d0fc3e10bca05ab6467525
edoardo.ascanio <edoardoascanio8@gmail.com>
2021-07-08 13:46:14 +0200

    add migration

--- database/migrations/2021_07_07_084345_create_posts_table.php
+++ database/migrations/2021_07_07_084345_create_posts_table.php
@@ -15,7 +15,8 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string("title", 255);
+            $table->longText("post")->nullable();
         });
     }
 
